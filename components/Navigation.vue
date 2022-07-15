b04265c759d089ac2977ffdce8e835d533ac94f3
van_thy <coder.haibach@gmail.com>
2022-07-15 12:00:59 +0700

    Update link menu

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,114 +1,7 @@
 <script setup>
 const menus = ref({
-  'Slider': {
-    'List templates': '/slider-list-templates',
-    'Layout': {
-      'Basic': '/slider-layout-basic',
-      'Classic 1': '/slider-layout-classic1',
-      'Classic 2': '/slider-layout-classic2',
-      'Center': '/slider-layout-center',
-      'Carousel': '/slider-layout-carousel',
-      'Outbox': '/slider-layout-outbox',
-      'Device': '/slider-layout-device',
-      'Coverscreen': '/slider-layout-coverscreen',
-      'Fullwidth': '/slider-layout-fullwidth',
-      'Fullwidth Range': '/slider-layout-fullwidthRange',
-      'Vertical Thumbnail': '/slider-layout-verticalThumb',
-      'Nested': '/slider-layout-nested',
-      'Multi': '/slider-layout-multi',
-      'Caption': '/slider-layout-caption',
-      'Height Auto': '/slider-layout-heightAuto',
-      'Height Fixed': '/slider-layout-heightFixed',
-    },
-    'Effect': {
-      'Line Effect': '/slider-effect-lin',
-      'Fade Effect': '/slider-effect-fade',
-      'Math Effect': '/slider-effect-math',
-      'CSS One': '/slider-effect-cssOne',
-      'CSS Two': '/slider-effect-cssTwo',
-      'CSS Four': '/slider-effect-cssFour',
-      'Coverflow3D': '/slider-effect-coverflow3D',
-      'Effect Random': '/lider-effect-random',
-      'Effect Control': '/slider-effect-control',
-    },
-    'Slideshow': {
-      'Arc Timer': '/slider-slideshow-timerArc',
-      'Line Timer': '/slider-slideshow-timerLine',
-      'Timer Only': '/slider-slideshow-timerOnly',
-      'Slideshow Random': '/slider-slideshow-random',
-    },
-    'API': {
-      'Add-Remove Tabs': '/slider-api-addremove',
-      'Callback Event': '/slider-api-callback',
-      'Show By Device': '/slider-api-showByDevic',
-      'Show In Range': '/slider-api-showInRange',
-      'Deep Linking': '/slider-api-deeplinking',
-      'Deep Linking Multi': '/slider-api-deeplinkingMulti',
-      'Cookie': '/slider-api-cookie',
-    }
-  },
-  'Tabs': {
-    'List templates': '/tabs-list-templates',
-    'Layout': {
-      'Device': '/tabs-layout-device',
-      'Vertical': '/tabs-layout-vertical',
-      'Nested': '/tabs-layout-nested',
-      'Bullet': '/tabs-layout-bullet',
-      'Fullwidth': '/tabs-layout-fullwidth',
-      'Page': '/tabs-layout-page',
-    },
-    'Effect': {
-      'Line': '/tabs-effect-line',
-      'Fade': '/tabs-effect-fade',
-      'CSS One': '/tabs-effect-cssOne',
-      'CSS Two': '/tabs-effect-cssTwo',
-      'CSS Four': '/tabs-effect-cssFour',
-      'None': '/tabs-effect-none',
-      'Effect Random': '/tabs-effect-random',
-      'Effect Control': '/tabs-effect-control',
-    },
-    'Style': {
-      'Flat': '/tabs-style-flat',
-      'Flatbox': '/tabs-style-flatbox',
-      'Outline': '/tabs-style-round.',
-      'Underline': '/tabs-style-underline',
-      'Size Of Style': '/tabs-style-size',
-      'Highlight Slide': '/tabs-style-highlight',
-    },
-    'Position': {
-      'Hor Begin-Begin': '/tabs-pos-hor-beginBegin',
-      'Hor Begin-Center': '/__tabs-pos-hor-beginCente__',
-      'Hor Begin-End': '/tabs-pos-hor-beginEnd',
-      'Hor Begin-Justify': '/tabs-pos-hor-beginJustify',
-      'Hor End-Begin': '/tabs-pos-hor-endBegin.',
-      'Hor End-Center': '/tabs-pos-hor-endCenter',
-      'Hor End-End': '/tabs-pos-hor-endEnd',
-      'Hor End-Justify': '/tabs-pos-hor-endJustify',
-      'Ver Begin-Begin': '/tabs-pos-ver-beginBegin',
-      'Ver Begin-Center': '/tabs-pos-ver-beginCenter',
-      'Ver Begin-End': '/tabs-pos-ver-beginEnd',
-      'Ver End-Begin': '/tabs-pos-ver-endBegin',
-      'Ver End-Center': '/tabs-pos-ver-endCenter',
-      'Ver End-End': '/tabs-pos-ver-endEnd',
-    },
-  },
-  'Tools': {
-    'Create CSS effect': '/page-create-css-effect',
-    'Preview CSS effect': '/page-preview-css-effect',
-  },
-  'Docs': {
-    to: '/documentation',
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16"><path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/></svg>',
-  },
-  'Download': {
-    to: '/download',
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>',
-  },
-})
-
-
-const links = ref({
-  'Slider': [
+  'Slider':
+  [
     {
       name: 'List templates',
       to: 'slider-list-templates'
@@ -174,7 +67,9 @@ const links = ref({
       ]
     }
   ],
-  'Tabs': [
+
+  'Tabs':
+  [
     {
       name: 'List templates',
       to: '/tabs-list-templates',
@@ -234,7 +129,9 @@ const links = ref({
       ]
     },
   ],
-  'Tools': [
+
+  'Tools':
+  [
     {
       name: 'Create CSS effect',
       to: '/page-create-css-effect',
@@ -244,11 +141,15 @@ const links = ref({
       to: '/page-preview-css-effect',
     }
   ],
-  'Docs': {
+
+  'Docs':
+  {
     to: '/documentation',
     icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16"><path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/></svg>',
   },
-  'Download': {
+
+  'Download':
+  {
     to: '/download',
     icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>',
   },
