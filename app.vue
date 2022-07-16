8be8154f74468fe444e4a8ce80945aa04699c09b
Nguyễn Văn Thy <coder.haibach@gmail.com>
2022-07-16 10:33:24 +0700

    Update font

--- app.vue
+++ app.vue
@@ -9,7 +9,10 @@ useHead({
     { name: 'description', content: 'Best slider and tabs jquery plugin' },
   ],
   link: [
-    { rel: 'icon', href: "/favicon.ico" }
+    { rel: 'icon', href: "/favicon.ico" },
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700;800&family=Raleway:wght@100;300&family=Source+Code+Pro:wght@400;500;700&display=swap' },
   ],
   script: [
     { src: './assets/js/jquery.min.js', mode: 'client' },
