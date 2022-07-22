4d107519ace948af9283f38468ccf400e2f41a46
van_thy <coder.haibach@gmail.com>
2022-07-22 16:21:14 +0700

    Add titleHead component + outbox layout page

---
+++ components/TitleHead.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  title: String
+})
+</script>
+
+
+<template>
+  <div class="title-head">
+    <div class="title-head-inner">
+      <h1>{{ props.title }}</h1>
+    </div>
+  </div>
+</template>
---
+++ pages/slider/layout-outbox.vue
@@ -0,0 +1,40 @@
+<script setup>
+const sliderOptions = {
+  fx: 'line',
+  speed: 600,
+  widthSlide: 778,
+  isPag: false,
+  isLoop: false,
+  isCenter: false,
+  margin: 10,
+}
+
+onMounted(() => {
+  setTimeout(() => {
+    const $slider = jQuery('#layout-outbox__slider')
+    if ($slider.length) {
+      $slider.rubyslider( sliderOptions )
+    }
+  }, 400)
+})
+</script>
+
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <TitleHead title="OUTBOX LAYOUT" />
+
+      <div id="layout-outbox__slider" class="slider-preview rs01">
+        <a class="rs01imgback" href="/img/season-landscape2.jpg">Season 2</a>
+        <a class="rs01imgback" href="/img/season-landscape3.jpg">Season 3</a>
+        <a class="rs01imgback" href="/img/season-landscape4.jpg">Season 4</a>
+        <a class="rs01imgback" href="/img/season-landscape5.jpg">Season 5</a>
+        <a class="rs01imgback" href="/img/season-landscape6.jpg">Season 6</a>
+        <a class="rs01imgback" href="/img/season-landscape7.jpg">Season 7</a>
+        <a class="rs01imgback" href="/img/season-landscape8.jpg">Season 8</a>
+      </div> <!-- /.rs01 -->
+    </div>
+  </div>
+</template>
+ 
