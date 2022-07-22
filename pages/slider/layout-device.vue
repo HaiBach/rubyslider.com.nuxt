4db402b48c9a33e065476f0b3b789ea42031da5a
Nguyễn Văn Thy <coder.haibach@gmail.com>
2022-07-22 22:21:22 +0700

    Update layout device

--- pages/slider/layout-device.vue
+++ pages/slider/layout-device.vue
@@ -24,11 +24,11 @@ onMounted(() => {
       <TitleHead title="DEVICE LAYOUT" />
       
       <div class="layout-device">
-        <div class="device">
+        <div class="device layout-device__bg">
           <img src="/img/ipad-landscape.png" alt="iPad device">
         </div>
 
-        <div id="layout-device__slider" class="slider-preview rs01">
+        <div id="layout-device__slider" class="layout-device__slider rs01">
           <div class="rs01slide">
             <a class="rs01imgback" href="/img/vietnam-large1.jpg">Việt Nam 1</a>
           </div>
@@ -54,7 +54,7 @@ onMounted(() => {
 </template>
 
 
-<style scoped>
+<!-- <style scoped>
 .layout-device {
   position: relative;
   margin-bottom: 100px;
@@ -107,6 +107,79 @@ onMounted(() => {
   .slider-preview {
     position: static;
     width: auto;
+  }
+}
+</style> -->
+
+<style lang="scss">
+.layout-device {
+  position: relative;
+  margin-bottom: 100px;
+  &__bg {
+    overflow: hidden;
+    height: 550px;
+    box-shadow: inset 0 -10px 25px -20px hsla(0,0%,0%,.5);
+    border-bottom: 1px solid #f9f9f9;
+    img {
+      display: block;
+      width: 100%;
+      height: auto;
+    }
+  }
+  .layout-device__slider {
+    position: absolute;
+    left: 112px;
+    top: 40px;
+    width: 914px;
+    .rs01slide {
+      border-radius: 0;
+    }
+    .rs01thumbnail {
+      .rs01pagitem {
+        width: 100px;
+        height: 70px;
+      }
+    }
+  }
+
+  /** MEDIA */
+  @media only screen and (max-width: 1199px) {
+    .layout-device__slider {
+      left: 92px;
+      top: 33px;
+      width: 754px;
+      .rs01thumbnail {
+        .rs01pagitem {
+          width: 80px;
+          height: 60px;
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 959px) {
+    &__bg {
+      height: 440px;
+    }
+    .layout-device__slider {
+      left: 73px;
+      top: 26px;
+      width: 600px;
+      .rs01thumbnail {
+        .rs01pagitem {
+          width: 70px;
+          height: 50px;
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 767px) {
+    &__bg {
+      display: none;
+    }
+    .layout-device__slider {
+      position: static;
+      width: auto;
+    }
   }
 }
 </style>
