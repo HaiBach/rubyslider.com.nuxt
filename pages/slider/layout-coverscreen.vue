e15f80c98de68b73797b1bcb46cdd0caf7a4a64c
Nguyễn Văn Thy <coder.haibach@gmail.com>
2022-07-23 16:28:42 +0700

    Update layout cover-screen

---
+++ pages/slider/layout-coverscreen.vue
@@ -0,0 +1,112 @@
+<script setup>
+const sliderOptions = {
+  fx: 'randomMath',
+  speed: 6000,
+  width: 940,
+  imagePosition: 'fill',
+  isSwipe: false,
+  isPag: false,
+  isFullscreen: true,
+  offsetBy: '.header',
+  isSlideshow: true,
+  slideshow: {
+    delay: 3000,
+    timer: 'line',
+    isPlayPause: true
+  }
+}
+
+onMounted(() => {
+  setTimeout(() => {
+    const $slider = jQuery('#layout-coverscreen__slider')
+    if ($slider.length) {
+      $slider.rubyslider( sliderOptions )
+    }
+  }, 400)
+})
+</script>
+
+
+<template>
+  <div>
+    <div class="layout-coverscreen">
+      <div id="layout-coverscreen__slider" class="slider-preview rs01">
+        <a class="rs01imgback" href="/img/vietnam-large1.jpg">Việt Nam 1</a>
+        <a class="rs01imgback" href="/img/vietnam-large2.jpg">Việt Nam 2</a>
+        <a class="rs01imgback" href="/img/vietnam-large3.jpg">Việt Nam 3</a>
+        <a class="rs01imgback" href="/img/vietnam-large4.jpg">Việt Nam 4</a>
+        <a class="rs01imgback" href="/img/vietnam-large5.jpg">Việt Nam 5</a>
+        <a class="rs01imgback" href="/img/vietnam-large6.jpg">Việt Nam 6</a>
+      </div> <!-- /.rs01 -->
+
+      <div class="main-content">
+          <div class="content-inner">
+              <h1>COVER-SCREEN LAYOUT</h1>
+          </div>
+      </div>
+    </div>
+
+    <div class="wrapper">
+      <div class="container">
+        <h2 style="text-align: center;">OTHER CONTENT</h2>
+        
+        <div class="font-blokk">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt id incidunt officia facere nisi, molestias blanditiis explicabo placeat nemo eveniet sapiente excepturi repellat amet deleniti magni similique quaerat earum corporis?</p>
+            <p>Placeat commodi, quod fugit adipisci consectetur voluptatum? Ex laudantium eum quisquam beatae illum velit nostrum, consectetur voluptatibus quis eligendi cum laboriosam aperiam odio necessitatibus suscipit ipsa tenetur, magni, harum ut!</p>
+            <p>Dolor recusandae quae, quam fuga, illum natus, cumque vero placeat alias repellendus reiciendis aspernatur delectus magni odio optio nesciunt ipsa quos dolores odit enim blanditiis iste voluptatum consequatur at. Temporibus?</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss">
+.layout-coverscreen {
+  overflow: hidden;
+  position: relative;
+  min-height: 600px;
+  background-color: #f0f0f0;
+  .slider-preview {
+    // z-index: -1;
+    // &::after {
+    //   content: '';
+    //   display: block;
+    //   position: absolute;
+    //   left: 0; top: 0;
+    //   width: 100%; height: 100%;
+    //   background: transparent url(/img/overlay-boken.png) center center;
+    //   opacity: 0.5;
+    //   z-index: 99999;
+    // }
+    .rs01slide {
+      border-radius: 0;
+    }
+    .rs01ss-control {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+    }
+    .rs01timeritem {
+      background-color: #000;
+    }
+  }
+  .main-content {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    padding: 30px;
+    background-color: hsla(0,0%,100%,.80);
+    border-radius: 8px;
+    box-shadow: 0 0 2px hsla(0,0%,0%,.25);
+    text-align: center;
+    transform: translate(-50%, -50%);
+    backdrop-filter: saturate(180%) blur(20px);
+    h1 {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
+ 
--- pages/slider/layout-device.vue
+++ pages/slider/layout-device.vue
@@ -54,63 +54,6 @@ onMounted(() => {
 </template>
 
 
-<!-- <style scoped>
-.layout-device {
-  position: relative;
-  margin-bottom: 100px;
-}
-.device {
-  overflow: hidden;
-  height: 550px;
-  box-shadow: inset 0 -10px 25px -20px hsla(0,0%,0%,.5);
-  border-bottom: 1px solid #f9f9f9;
-}
-.device > img {
-  display: block;
-  width: 100%;
-  height: auto;
-}
-/* SLIDER PREVIEW */
-.slider-preview {
-  position: absolute;
-  left: 112px;
-  top: 40px;
-  width: 914px;
-}
-.slider-preview .rs01slide {
-  border-radius: 0;
-}
-.slider-preview .rs01thumbnail .rs01pagitem {
-  width: 100px;
-  height: 70px;
-}
-
-/* MEDIA WIDTH */
-@media only screen and (min-width: 768px) and (max-width: 959px) {
-  .device {
-    height: 440px;
-  }
-  .slider-preview {
-    left: 73px;
-    top: 26px;
-    width: 600px;
-  }
-  .slider-preview .rs01thumbnail .rs01pagitem {
-    width: 80px;
-    height: 60px;
-  }
-}
-@media only screen and (max-width: 767px) {
-  .device {
-    display: none;
-  }
-  .slider-preview {
-    position: static;
-    width: auto;
-  }
-}
-</style> -->
-
 <style lang="scss">
 .layout-device {
   position: relative;
