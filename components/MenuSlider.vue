2aaaed6f6c96ca56f56979daaf9bb1f3b30d4ba1
Nguyễn Văn Thy <coder.haibach@gmail.com>
2022-08-11 01:27:33 +0700

    Increase height for header

--- components/MenuSlider.vue
+++ components/MenuSlider.vue
@@ -48,16 +48,14 @@ onMounted(() => {
       <div class="megamenu__slide rs01slide">
 
         <div class="megamenu--link-lv2 rs01pagitem" :data-slide-num="indexLevel2">
-          <a>
-            <i v-if="menuLevel2.icon" v-html="menuLevel2.icon "></i>
-            <span>{{ menuLevel2.name }}</span>
-            <span class="megamenu__caret" v-if="menuLevel2.menus"></span>
-            <i class="megamenu__icon_arrow">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-              </svg>
-            </i>
-          </a>
+          <i v-if="menuLevel2.icon" v-html="menuLevel2.icon "></i>
+          <span>{{ menuLevel2.name }}</span>
+          <span class="megamenu__caret" v-if="menuLevel2.menus"></span>
+          <i class="megamenu__icon_arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+            </svg>
+          </i>
         </div>
 
         <div v-if="menuLevel2.menus" class="megamenu__items">
