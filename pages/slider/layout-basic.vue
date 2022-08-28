2baf7dba2e4a56ee59f604403d92d791a006312c
Nguyễn Văn Thy <coder.haibach@gmail.com>
2022-08-28 22:36:54 +0700

    Complete classic 1 layout

--- pages/slider/layout-basic.vue
+++ pages/slider/layout-basic.vue
@@ -26,45 +26,48 @@ onMounted(() => {
 
 
 <template>
-  <main id="main">
-    <section class="layout-basic wrapper">
-      <div class="container">
-        <TitleHead>
-          <template #title>
-            <h1>LAYOUT CƠ BẢN</h1>
-          </template>
-        </TitleHead>
+<main id="main">
+  <section class="layout-basic wrapper">
+    <div class="container">
+      <TitleHead>
+        <template #title>
+          <h1>LAYOUT CƠ BẢN</h1>
+        </template>
+      </TitleHead>
 
-        <div class="layout-basic__slider slider-preview rs01">
-          <a class="rs01imgback" href="/img/vietnam-1140-1.jpg">Việt Nam 1</a>
-          <a class="rs01imgback" href="/img/vietnam-1140-2.jpg">Việt Nam 2</a>
-          <a class="rs01imgback" href="/img/vietnam-1140-4.jpg">Việt Nam 4</a>
-          <a class="rs01imgback" href="/img/vietnam-1140-5.jpg">Việt Nam 5</a>
-          <a class="rs01imgback" href="/img/vietnam-1140-6.jpg">Việt Nam 6</a>
-        </div>
+      <div class="layout-basic__slider slider-preview rs01">
+        <a class="rs01imgback" href="/img/vietnam-1140-1.jpg">Việt Nam 1</a>
+        <a class="rs01imgback" href="/img/vietnam-1140-2.jpg">Việt Nam 2</a>
+        <a class="rs01imgback" href="/img/vietnam-1140-4.jpg">Việt Nam 4</a>
+        <a class="rs01imgback" href="/img/vietnam-1140-5.jpg">Việt Nam 5</a>
+        <a class="rs01imgback" href="/img/vietnam-1140-6.jpg">Việt Nam 6</a>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section class="guide">
-      <div class="container">
-        <div class="hr hr--circle"></div>
-        <h2>❖ Giới thiệu</h2>
-        <p>Như tên gọi, là layout cơ bản và đơn giản nhất khi thiết lập RubySlider. Thiết lập hiệu ứng cssOne để slider có hiệu ứng ấn tượng hơn.<br>Mặc định thì navigation (button next/prev) được tắt đi, chỉ có pagination được hiển thị với các hình thumbnail nhỏ bên dưới.</p>
-        <ul>
-          <li>Hiệu ứng mặc định của plugin RubySlider là 'Line'.</li>
-        </ul>
+  <section class="guide">
+    <div class="container">
+      <div class="hr hr--circle"></div>
+      <h2>❖ Giới thiệu</h2>
+      <p>Như tên gọi, là layout cơ bản và đơn giản nhất khi thiết lập RubySlider. Thiết lập hiệu ứng cssOne để slider có hiệu ứng ấn tượng hơn.<br>Mặc định thì navigation (button next/prev) được tắt đi, chỉ có pagination được hiển thị với các hình thumbnail nhỏ bên dưới.</p>
+      <ul>
+        <li>Hiệu ứng mặc định của plugin RubySlider là 'Line'.</li>
+      </ul>
 
-        <LineSpace />
+      <LineSpace />
 
-        <h2>❖ Thiết lập slider</h2>
-        <p>Các bạn có thể được hướng dẫn thiết lập slider chi tiết ở trang <NuxtLink to="/documentation">hướng dẫn
-          </NuxtLink> này.<br>Dưới đây là options hiện tại của slider phía trên:</p>
-        <pre class="codeprint">{{ sliderOptions }}</pre>
+      <h2>❖ Thiết lập slider</h2>
+      <p>Các bạn có thể được hướng dẫn thiết lập slider chi tiết ở trang <NuxtLink to="/documentation">hướng dẫn
+        </NuxtLink> này.<br>Dưới đây là options hiện tại của slider phía trên:</p>
+      <pre class="codeprint">{{ sliderOptions }}</pre>
 
-        <ButtonPrevNext prev-name="Tất cả ví dụ" prev-to="/slider" next-name="Layout cổ điển 1"
-          next-to="/slider/layout-classic-1" />
-      </div>
-    </section>
-  </main>
+      <ButtonPrevNext
+        prev-name="Tất cả ví dụ"
+        prev-to="/slider"
+        next-name="Layout cổ điển 1"
+        next-to="/slider/layout-classic-1" />
+    </div>
+  </section>
+</main>
 </template>
  
--- pages/slider/layout-classic-1.vue
+++ pages/slider/layout-classic-1.vue
@@ -1,70 +1,122 @@
 <script setup>
 const sliderOptions = {
+  // name: 'layoutClassic1',
   fx: 'line',
   speed: 600,
   width: 1140,
-  height: 600,
-  margin: 10,
-  imagePosition: 'fill',
+  // height: 600,
+  margin: 5,
   pag: {
     type: 'bullet',
-    isMark: true,
   },
   isNav: true
 }
+const initSetup = () => {
+  jQuery('#main .rs01nav').remove()
+  jQuery('#main .rs01pag').remove()
+  jQuery('#main .rs01overlay-ghost').remove()
+  jQuery('#main .rs01imgback').css({ width: '', height: '', left: '', top: '' })
 
+  const rubyLayoutClassic = jQuery('.layout-classic1__slider').rubyslider( sliderOptions )
+}
 onMounted(() => {
-  setTimeout(() => {
-    const $slider = jQuery('#layout-classic__slider')
-    if ($slider.length) {
-      $slider.rubyslider( sliderOptions )
-    }
-  }, 400)
+  setTimeout(initSetup, 400)
 })
 </script>
 
 
 <template>
-  <div class="wrapper">
+<main id="main">
+  <section class="layout-classic1 wrapper">
     <div class="container">
-
-      <div class="title-head">
-        <div class="title-head-inner">
-          <h1>CLASSIC 1 LAYOUT</h1>
+      <TitleHead>
+        <template #title>
+          <h1>LAYOUT CỔ ĐIỂN SỐ 1</h1>
+        </template>
+      </TitleHead>
+        
+      <div class="layout-classic1__slider slider-preview rs01">
+        <div class="rs01viewport">
+          <a class="rs01imgback" href="/img/vietnam-1140-1.jpg">Việt Nam 1</a>
+          <a class="rs01imgback" href="/img/vietnam-1140-2.jpg">Việt Nam 2</a>
+          <a class="rs01imgback" href="/img/vietnam-1140-4.jpg">Việt Nam 4</a>
+          <a class="rs01imgback" href="/img/vietnam-1140-5.jpg">Việt Nam 5</a>
+          <a class="rs01imgback" href="/img/vietnam-1140-6.jpg">Việt Nam 6</a>
         </div>
+        <div class="rs01shadow"></div>
       </div>
-
-      <div id="layout-classic__slider" class="slider-preview rs01 ruby-mark">
-        <a class="rs01imgback" href="/img/vietnam-large1.jpg">Việt Nam 1</a>
-        <a class="rs01imgback" href="/img/vietnam-large2.jpg">Việt Nam 2</a>
-        <a class="rs01imgback" href="/img/vietnam-large3.jpg">Việt Nam 3</a>
-        <a class="rs01imgback" href="/img/vietnam-large4.jpg">Việt Nam 4</a>
-        <a class="rs01imgback" href="/img/vietnam-large5.jpg">Việt Nam 5</a>
-        <a class="rs01imgback" href="/img/vietnam-large6.jpg">Việt Nam 6</a>
-      </div>
-
     </div>
-  </div>
+  </section>
+
+  <section class="guide">
+    <div class="container">
+      <div class="hr hr--circle"></div>
+      <h2>❖ Giới thiệu</h2>
+      <p>Layout cổ điển số 1 được thiết lập hiệu ứng Line đơn giản.<br>Slider được hiển thị với navigation (button next/prev) và pagination kiểu bullet, đặc biệt kết hợp với shadow ở dưới slider để thể hiện các slider có phong cách cổ điển 1 thời.</p>
+      <ul>
+        <li>Hiệu ứng mặc định của plugin RubySlider là <b>Line</b>.</li>
+      </ul>
+
+      <LineSpace />
+
+      <h2>❖ Thiết lập slider</h2>
+      <p>Các bạn có thể được hướng dẫn thiết lập slider chi tiết ở trang <NuxtLink to="/documentation">hướng dẫn
+        </NuxtLink> này.<br>Dưới đây là options hiện tại của slider phía trên:</p>
+      <pre class="codeprint">{{ sliderOptions }}</pre>
+
+      <ButtonPrevNext
+        prev-name="Layout cơ bản"
+        prev-to="/slider/layout-basic"
+        next-name="Layout cổ điển 2"
+        next-to="/slider/layout-classic-2" />
+    </div>
+  </section>
+</main>
 </template>
 
 
-<style scoped>
-.container {
-  overflow: hidden;
-}
-.slider-preview .rs01pag {
-  position: absolute;
-  bottom: 0;
-}
-
-.slider-shadow img {
-  display: block;
-  width: 100%; height: auto;
-}
-
-.ruby-mark .rs01pagmark-item {
-  background-color: #fff;
-  border-color: #fff;
+<style lang="scss">
+.layout-classic1 {
+  .rs01 {
+    .rs01pag {
+      position: absolute;
+      bottom: 0;
+      padding-bottom: 15px;
+      .rs01pagmark-item {
+        height: 10px;
+      }
+    }
+    .rs01paginner {
+      &::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: -5px;
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        background-color: rgba($color: #fff, $alpha: .65);
+        border-radius: 100px;
+      }
+    }
+    .rs01pagitem {
+      width: 10px;
+      height: 10px;
+      background-color: rgba($color: #000, $alpha: .75);
+    }
+    &.rs01ready {
+      .rs01shadow {
+        visibility: visible;
+      }
+    }
+  }
+  .rs01shadow {
+    visibility: hidden;
+    width: 100%;
+    height: 46px;
+    background: url(/img/shadow.png) no-repeat center center;
+    background-size: cover;
+  }
 }
 </style>
  
