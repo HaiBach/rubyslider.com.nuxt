76003ae48531518c231111a60a9c50cc09d5ccf2
van_thy <coder.haibach@gmail.com>
2022-08-29 15:59:07 +0700

    Update layout center

--- pages/slider/layout-center.vue
+++ pages/slider/layout-center.vue
@@ -1,62 +1,78 @@
 <script setup>
+definePageMeta({
+  middle
+})
 const sliderOptions = {
+  // fx: 'coverflow3D',
   fx: 'line',
   speed: 600,
   width: 1140,
-  height: 600,
-  // widthSlide: [
-  //   '70%',
-  //   [720, 768, 959],
-  //   [940, 960, 10000]
-  // ],
-  // margin: [
-  //   [5, 0, 479],
-  //   10,
-  //   [20, 960, 10000]
-  // ],
   widthSlide: [1140, 940, '70%'],
-  margin: [20, 10, 10, 5],
-  // imagePosition: 'fill',
+  margin: [10, 5, 5],
 }
+const initSetup = () => {
+  jQuery('#main .rs01nav').remove()
+  jQuery('#main .rs01pag').remove()
+  jQuery('#main .rs01overlay-ghost').remove()
+  jQuery('#main .rs01imgback').css({ width: '', height: '', left: '', top: '' })
 
+  const rubyLayoutCenter = jQuery('.layout-center__slider').rubyslider( sliderOptions )
+}
 onMounted(() => {
-  setTimeout(() => {
-    const $slider = jQuery('#layout-center__slider')
-    if ($slider.length) {
-      $slider.rubyslider( sliderOptions )
-    }
-  }, 400)
+  setTimeout(initSetup, 400)
 })
 </script>
 
 
 <template>
-  <div class="wrapper">
+<main id="main">
+  <section class="layout-center wrapper">
     <div class="container">
-
-      <div class="title-head">
-        <div class="title-head-inner">
-          <h1>CENTER LAYOUT</h1>
-        </div>
-      </div>
-
-      <div id="layout-center__slider" class="slider-preview rs01">
-        <a class="rs01imgback" href="/img/vietnam-large1.jpg">Việt Nam 1</a>
-        <a class="rs01imgback" href="/img/vietnam-large2.jpg">Việt Nam 2</a>
-        <a class="rs01imgback" href="/img/vietnam-large3.jpg">Việt Nam 3</a>
-        <a class="rs01imgback" href="/img/vietnam-large4.jpg">Việt Nam 4</a>
-        <a class="rs01imgback" href="/img/vietnam-large5.jpg">Việt Nam 5</a>
-        <a class="rs01imgback" href="/img/vietnam-large6.jpg">Việt Nam 6</a>
-      </div>
-
+      <TitleHead>
+        <template #title>
+          <h1>LAYOUT CHÍNH GIỮA</h1>
+        </template>
+      </TitleHead>
     </div>
-  </div>
+
+    <div class="layout-center__slider slider-preview rs01">
+      <a class="rs01imgback" href="/img/vietnam-1140-1.jpg">Việt Nam 1</a>
+      <a class="rs01imgback" href="/img/vietnam-1140-2.jpg">Việt Nam 2</a>
+      <a class="rs01imgback" href="/img/vietnam-1140-4.jpg">Việt Nam 4</a>
+      <a class="rs01imgback" href="/img/vietnam-1140-5.jpg">Việt Nam 5</a>
+      <a class="rs01imgback" href="/img/vietnam-1140-6.jpg">Việt Nam 6</a>
+    </div>
+  </section>
+
+  <section class="guide">
+    <div class="container">
+      <div class="hr hr--circle"></div>
+      <h2>❖ Giới thiệu</h2>
+      <p>Hiệu ứng được sử dụng trong layout chính giữa là hiệu ứng <b>Line</b>, hoặc <b>Coverflow3D</b>. trong trường hợp này là hiệu ứng Line.<br>
+      Layout được sử dụng khi cần sử dụng slider có chiều dài lớn hơn chiều dài của slide.</p>
+      <ul>
+        <li>Cần ít nhất 3 hình ảnh để có layout chính giữa.</li>
+      </ul>
+      
+      <LineSpace />
+
+      <h2>❖ Thiết lập slider</h2>
+      <p>Các bạn có thể được hướng dẫn thiết lập slider chi tiết ở trang <NuxtLink to="/documentation">hướng dẫn</NuxtLink> này.<br>Dưới đây là options hiện tại của slider phía trên:</p>
+      <pre class="codeprint">{{ JSON.stringify(sliderOptions, null, 4) }}</pre>
+
+      <ButtonPrevNext
+        prev-name="Layout cổ điển số 2"
+        prev-to="/slider/layout-classic-2"
+        next-name="Layout băng chuyền"
+        next-to="/slider/layout-carousel" />
+    </div>
+  </section>
+</main>
 </template>
 
 
-<style scoped>
-.wrapper {
-  overflow: hidden;
+<style lang="scss">
+.layout-center {
 }
 </style>
  
